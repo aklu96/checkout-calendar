82ac283ef6c6a402f9cc1b25afbe464f278a3f59
Alex Klyuev <aklyuev96@gmail.com>
2021-02-05 20:23:00 -0800

    create database schema sql file

--- database/Schema.sql
+++ database/Schema.sql
@@ -5,8 +5,25 @@ CREATE DATABASE listings;
 USE listings;
 
 CREATE TABLE listings (
-  id INTEGER AUTO_INCREMENT,
-  max_guests
-  price
-  owner_id
-)
+  listing_id INT GENERATED ALWAYS AS IDENTITY,
+  max_guests INT,
+  price DECIMAL,
+  owner_email VARCHAR(100),
+  cleaning_fee VARCHAR(20),
+  service_fee VARCHAR(20),
+  min_stay INT,
+  PRIMARY KEY (id),
+);
+
+CREATE TABLE reservations (
+  id INT AUTO_INCREMENT,
+  renter_email VARCHAR(100),
+  check_in DATE,
+  check_out DATE,
+  total_cost DECIMAL,
+  num_adults INT,
+  num_children, INT,
+  num_infants INT,
+  listing_id INT,
+  FOREIGN KEY (listing_id) REFERENCES listings(id)
+);
